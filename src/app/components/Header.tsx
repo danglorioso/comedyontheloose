ec3fc7a7746317a0694062c84bf94bf85637cd06
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-06-07 00:11:46 -0400

    Matching nav links

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,15 +6,17 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="bg-white text-white p-4 shadow-md border-t-4 border-green-500">
-      <div className="max-w-screen-xl mx-auto flex justify-between items-center">
+      <div className="max-w-screen-lg mx-auto flex justify-between items-center">
         <div className="flex items-center gap-2">
           <img src="/logo.png" alt="Comedy on the Loose" className="h-22 w-auto" />
         </div>
         <nav className="space-x-4 text-sm text-black font-semibold">
+          <a href="" className="hover:text-green-500">Home</a>
           <a href="#about" className="hover:text-green-500">About Us</a>
+          <a href="#gallery" className="hover:text-green-500">Gallery</a>
+          <a href="#episodes" className="hover:text-green-500">Online Episodes</a>
           <a href="#sketches" className="hover:text-green-500">Sketches</a>
-          <a href="#watch" className="hover:text-green-500">Watch</a>
-          <a href="#news" className="hover:text-green-500">Newsletter</a>
+          <a href="#schedule" className="hover:text-green-500">Schedule</a>
           <a href="#contact" className="hover:text-green-500">Contact</a>
         </nav>
       </div>
