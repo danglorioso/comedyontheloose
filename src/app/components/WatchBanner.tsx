7267a5d410dbdbe96f81e7f097a187bd67f809fa
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-06-23 02:13:32 -0400

    Improved Watch Us banner

--- src/app/components/WatchBanner.tsx
+++ src/app/components/WatchBanner.tsx
@@ -5,8 +5,8 @@ import { FaPlayCircle, FaCalendarAlt } from "react-icons/fa";
 
 export default function WatchUs() {
   return (
-    <section className="bg-[#3DBC27] py-3">
-      <div className="max-w-4xl mx-auto flex flex-col md:flex-row justify-center items-center text-white text-center md:text-left gap-8">
+    <section className="bg-[#3DBC27] py-5">
+      <div className="max-w-screen-xl mx-auto flex flex-col items-center justify-center md:flex-row text-white text-center md:text-left gap-8">
         {/* Watch Online */}
         <div className="flex flex-col md:flex-row items-center text-right gap-4">
           <h2 className="text-4xl font-bold leading-none">
@@ -14,10 +14,10 @@ export default function WatchUs() {
           </h2>
           <a
             href="#episodes"
-            className="flex items-center gap-2 bg-[#73B666] border border-white px-6 py-3 rounded-md font-bold text-white hover:bg-[#6AC954] transition-colors"
+            className="flex items-center justify-center gap-2 bg-[#73b666] border border-white px-10 py-4 rounded-md font-bold text-white hover:bg-[#6ac954] transition-colors"
           >
-            <FaPlayCircle className="w-5 h-5" />
-            ONLINE EPISODES
+            <FaPlayCircle className="w-6 h-6"/>
+            <h2 className="text-2xl font-bold">ONLINE EPISODES</h2>
           </a>
         </div>
 
@@ -31,10 +31,10 @@ export default function WatchUs() {
           </h2>
           <a
             href="#schedule"
-            className="flex items-center gap-2 bg-[#D04D44] px-6 py-3 rounded-md font-bold text-white border border-white hover:bg-white hover:text-[#D04D44] transition-colors"
+            className="flex items-center justify-center gap-2 bg-[#D04D44] px-10 py-4 rounded-md font-bold text-white border border-white hover:bg-[#d57875] transition-colors"
           >
-            <FaCalendarAlt className="w-5 h-5" />
-            AIRING SCHEDULE
+            <FaCalendarAlt className="w-6 h-6" />
+            <h2 className="text-2xl font-bold">AIRING SCHEDULE</h2>
           </a>
         </div>
       </div>
