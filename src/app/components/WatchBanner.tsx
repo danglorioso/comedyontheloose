826ab832ec20e72a152a7159d49dfdf3f3e7d987
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-06-23 02:14:38 -0400

    Adding hover grows to buttons

--- src/app/components/WatchBanner.tsx
+++ src/app/components/WatchBanner.tsx
@@ -8,13 +8,13 @@ export default function WatchUs() {
     <section className="bg-[#3DBC27] py-5">
       <div className="max-w-screen-xl mx-auto flex flex-col items-center justify-center md:flex-row text-white text-center md:text-left gap-8">
         {/* Watch Online */}
-        <div className="flex flex-col md:flex-row items-center text-right gap-4">
+        <div className="flex flex-col md:flex-row items-center text-right gap-8">
           <h2 className="text-4xl font-bold leading-none">
             WATCH US<br />ONLINE:
           </h2>
           <a
             href="#episodes"
-            className="flex items-center justify-center gap-2 bg-[#73b666] border border-white px-10 py-4 rounded-md font-bold text-white hover:bg-[#6ac954] transition-colors"
+            className="flex items-center justify-center gap-2 bg-[#73b666] border border-white px-10 py-4 rounded-md font-bold text-white hover:bg-white hover:text-[#73b666] hover:border-[#73b666] transition-all duration-300 transform hover:scale-105"
           >
             <FaPlayCircle className="w-6 h-6"/>
             <h2 className="text-2xl font-bold">ONLINE EPISODES</h2>
@@ -25,13 +25,13 @@ export default function WatchUs() {
         <div className="hidden md:block h-16 w-px bg-white opacity-50" />
 
         {/* Watch on TV */}
-        <div className="flex flex-col md:flex-row items-center text-right gap-4">
+        <div className="flex flex-col md:flex-row items-center text-right gap-8">
           <h2 className="text-4xl font-bold leading-none">
             WATCH US<br />ON TV:
           </h2>
           <a
             href="#schedule"
-            className="flex items-center justify-center gap-2 bg-[#D04D44] px-10 py-4 rounded-md font-bold text-white border border-white hover:bg-[#d57875] transition-colors"
+            className="flex items-center justify-center gap-2 bg-[#D04D44] px-10 py-4 rounded-md font-bold text-white border border-white hover:bg-white hover:text-[#D04D44] hover:border-[#D04D44] transition-all duration-300 transform hover:scale-105"
           >
             <FaCalendarAlt className="w-6 h-6" />
             <h2 className="text-2xl font-bold">AIRING SCHEDULE</h2>
