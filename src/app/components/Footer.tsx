878b00841b9a7538e55c220cdef8a39f6be20aa2
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-06-29 02:55:58 -0400

    Fixing footer columns

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,36 +5,23 @@ import React from "react";
 export default function Footer() {
     return (
         <footer className="bg-slate-900 p-4 shadow-md border-t-4 border-[#3dbc27] mt-2">
-            <div className="max-w-screen-lg px-15 mx-auto flex gap-4 p-4">
-                <div className="basic-">
-                    <div className="text-2xl font-bold text-[#3dbc27]">
-                        <img src="/favicon.png" alt="Comedy on the Loose" className="h-22 w-auto" />
-                    </div>
+            <div className="max-w-screen-lg mx-auto grid grid grid-cols-5 items-start gap-4 p-4">
+                <div className="col-span-1 flex justify-center items-center">
+                    <img src="/favicon.png" 
+                         alt="Comedy on the Loose" 
+                         className="h-25 w-auto" />
                 </div>
-                <div className="basic-lg">
-                    <p className="text-4xl">
-                        About Us
-                    </p>
-                    <p className="text-sm text-gray-600 mt-2">
-                        Comedy on the Loose, created by Dan and Cam in 2016 and airing exclusively on Littleton Community Television, showcases their humorous journey from homemade videos to full-length episodes.
+                <div className="col-span-2 mr-6">
+                    <p className="text-3xl">About Us</p>
+                    <p className="text-md text-gray-400 mt-2">
+                    Comedy on the Loose, created by Dan and Cam in 2016, airs exclusively on Littleton Community Television and showcases their humorous journey from homemade videos to full-length, studio-produced episodes.
                     </p>
                 </div>
-                <div className="basic-lg">Column 3</div>
-                            
-                
-                
-                
-                {/* <div className="text-sm text-gray-600">
-                    &copy; {new Date().getFullYear()} Comedy on the Loose. All rights reserved.
+                <div className="col-span-2">
+                    <p className="text-3xl">Watch Us On</p>
                 </div>
-                <div className="text-sm text-gray-600">
-                    Follow us on{" "}
-                    <a href="https://www.youtube.com/@comedyontheloose" target="_blank" rel="noopener noreferrer" className="text-[#3dbc27] hover:underline">
-                        YouTube
-                    </a>
-                </div> */}
             </div>
-            <div className="text-center mt-4">
+            <div className="text-center mt-6">
                 <p className="mb-2 text-md text-slate-300 leading-tight">
                     Website created by{" "}
                     <a
