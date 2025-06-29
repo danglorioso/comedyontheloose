73019b66518a0f92b16089950f8ad6eac4e89feb
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-06-29 04:05:10 -0400

    Recentering footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,20 +4,20 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer className="bg-slate-900 p-4 shadow-md border-t-4 border-[#3dbc27] mt-2">
-            <div className="max-w-screen-lg mx-auto grid grid grid-cols-5 items-start gap-4 p-4">
-                <div className="col-span-1 flex justify-center items-center">
+        <footer className="bg-slate-900 p-4 shadow-md border-t-4 border-[#3dbc27]">
+            <div className="max-w-screen-lg mx-auto grid grid grid-cols-5 items-start gap-10 p-4">
+                <div className="col-span-1 flex justify-end items-end">
                     <img src="/favicon.png" 
                          alt="Comedy on the Loose" 
                          className="h-25 w-auto" />
                 </div>
-                <div className="col-span-2 mr-6">
+                <div className="col-span-2">
                     <p className="text-3xl">About Us</p>
                     <p className="text-md text-gray-400 mt-2">
                     Comedy on the Loose, created by Dan and Cam in 2016, airs exclusively on Littleton Community Television and showcases their humorous journey from homemade videos to full-length, studio-produced episodes.
                     </p>
                 </div>
-                <div className="col-span-2 mr-6">
+                <div className="col-span-2">
                     <p className="text-3xl">Watch Us On</p>
                     <div className="flex flex-row items-end gap-10 mt-2">
                         <a
