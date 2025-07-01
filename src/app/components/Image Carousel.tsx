1ee4992491843408ee34bad8941ccdf204ca810a
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-07-01 03:28:41 -0400

    First steps of image carousel

--- src/app/components/Image Carousel.tsx
+++ src/app/components/Image Carousel.tsx
@@ -1,29 +1,32 @@
 "use client";
 
-import React from "react";
+import React from 'react'
+import { CCarousel, CCarouselCaption, CCarouselItem, CImage } from '@coreui/react'
 
-export default function ImageCarousel() {
+export const CarouselWithCaptionsExample = () => {
   return (
-    <section className="bg-white py-8">
-      <div className="max-w-screen-xl px-16 mx-auto px-4">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-8 items-center mt-8">
-          {/* Text Column */}
-          <div>
-            <h2 className="text-4xl font-bold mb-8 text-[#55595e]">
-              IMAGE CAROUSEL
-            </h2>
-            <p className="text-lg text-gray-700 leading-relaxed mb-4">
-              This is a placeholder for an image carousel component.
-            </p>
-          </div>
-
-          {/* Image Column */}
-          <div className="flex justify-center lg:justify-end">
-            {/* Placeholder for image carousel */}
-            <div className="w-full max-w-md h-auto bg-gray-200 rounded-lg shadow-lg"></div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+    <CCarousel controls indicators>
+      <CCarouselItem>
+        <CImage className="d-block w-100" src={('/images/react.jpg')} alt="slide 1" />
+        <CCarouselCaption className="d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </CCarouselCaption>
+      </CCarouselItem>
+      <CCarouselItem>
+        <CImage className="d-block w-100" src={('/images/vue.jpg')} alt="slide 2" />
+        <CCarouselCaption className="d-none d-md-block">
+          <h5>Second slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </CCarouselCaption>
+      </CCarouselItem>
+      <CCarouselItem>
+        <CImage className="d-block w-100" src={('/images/angular.jpg')} alt="slide 3" />
+        <CCarouselCaption className="d-none d-md-block">
+          <h5>Third slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </CCarouselCaption>
+      </CCarouselItem>
+    </CCarousel>
+  )
 }
