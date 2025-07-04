ee34524e68e61c732929e74e18269f067ec15837
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-07-04 02:22:43 -0400

    Fixing ways to watch title bold

--- src/app/components/WaysToWatch.tsx
+++ src/app/components/WaysToWatch.tsx
@@ -8,7 +8,7 @@ export default function WaysToWatch() {
   return (
     <section className="bg-[#005395] py-12">
       <div className="max-w-screen-xl px-6 md:px-16 mx-auto">
-        <div className="text-4xl font-bold text-center text-white">WAYS TO WATCH</div>
+        <div className="text-4xl font-medium text-center text-white">WAYS TO WATCH</div>
 
         <div className="mt-12 grid grid-cols-1 md:grid-cols-2 gap-8">
           {/* Shared styles */}
