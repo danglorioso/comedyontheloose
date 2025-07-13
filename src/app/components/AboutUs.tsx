15ce9ee314b0d358f1815cedd7071e99f860755c
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-07-13 15:46:26 -0400

    New icon order

--- src/app/components/AboutUs.tsx
+++ src/app/components/AboutUs.tsx
@@ -167,9 +167,9 @@ export default function AboutUs() {
       {/* Footer Icons */}
         <footer className="flex justify-center space-x-6 mt-10 text-gray-600">
             <a href="http://bit.ly/SubToCOTL"><FaYoutube size={24} /></a>
+            <a href="http://bit.ly/COTLinsta"><FaInstagram size={24} /></a>
             <a href="http://bit.ly/COTLfacebook"><FaFacebook size={24} /></a>
             <a href="http://bit.ly/COTLtwitter"><FaTwitter size={24} /></a>
-            <a href="http://bit.ly/COTLinsta"><FaInstagram size={24} /></a>
         </footer>
 
         <div className="flex justify-center mt-4">
